0a7c715473485bc542ffc3b7b3d8e041a9da9fd4
César Rodrigues <cesar.nascimento.ufc@gmail.com>
2018-09-23 09:26:56 -0300

    Initial commit

---
+++ app/src/main/java/comcesar1287/github/walletstone/MainActivity.kt
@@ -0,0 +1,12 @@
+package comcesar1287.github.walletstone
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
